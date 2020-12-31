81cd61044927517c47aac90014d3ae87ff891c34
Michael <mailmichaeltr@gmail.com>
2020-12-31 01:46:25 -0500

    Updated Capacitance Calculation Function

--- JavaScript/Capacitor_Calculator.js
+++ JavaScript/Capacitor_Calculator.js
@@ -35,8 +35,10 @@ function Update_Tolerance(){
     Capacitor_Code = Capacitor_Code + String(Code_Buttons[Button_Index].value);
   }
   
+  var Tolerance_Letter = String(Tolerance_Code[parseInt(Slider.value)])
   var Capacitor_Value_Label = document.getElementById('Capacitor_Value');
-  Capacitor_Value_Label.innerHTML = Capacitor_Code + String(Tolerance_Code[parseInt(Slider.value)]);;
+  Capacitor_Value_Label.innerHTML = Capacitor_Code + Tolerance_Letter;
+  Calculate_Tolerance(Tolerance_Letter);
   }
 
 function Update_Capacitor_Preview(){
@@ -45,8 +47,9 @@ function Update_Capacitor_Preview(){
     var Valid_Keys = [48,49,50,51,52,53,54,55,56,57];
     var Current_Text_Element = parseInt(document.getElementById(this.id).value);
     
-    if(!Valid_Keys.includes(Key_Pressed)){
-        event.preventDefault();
+    if(isNaN(Current_Text_Element)){
+      document.getElementById(this.id).value = "";
+      // event.preventDefault();
     }    
 
     var Capacitor_Code = "";
@@ -58,18 +61,63 @@ function Update_Capacitor_Preview(){
     var Capacitor_Value_Label = document.getElementById('Capacitor_Value');
     var Tolerance_Code = String(document.getElementById('Selected_Label').innerHTML);
     Capacitor_Value_Label.innerHTML = Capacitor_Code + Tolerance_Code;
+    Calculate_Capacitance(Capacitor_Code)
 }
 
-
-function Calculate_Capacitance(){
+function Calculate_Capacitance(Capacitor_Code){
+  var Significant_Digits = Capacitor_Code.substring(0,2);
+  if(Capacitor_Code.length <= 2){
+    var Trailing_Zeroes_Factor = 0;
+    var Trailing_Zeroes_String = "";
+  }
+  else{
+    var Trailing_Zeroes_Factor = parseInt(Capacitor_Code.split("").pop());
+    var Trailing_Zeroes_String = String(10**Trailing_Zeroes_Factor);
+    Trailing_Zeroes_String = Trailing_Zeroes_String.substring(1,Trailing_Zeroes_String.length);
+  }
   
-
+  var Capacitance_In_Picofarads = Significant_Digits + Trailing_Zeroes_String;
+  console.log(Capacitance_In_Picofarads);
+  
+  /*Three maximum digits*/
+  // if(){
+  // 
+  //   var Unit_String = "";
+  // }
+  // 
+  // if(){
+  // 
+  //   var Unit_String = "";
+  // }
+  // 
+  // if(){
+  // 
+  //   var Unit_String = "";
+  // }
+  // 
+  // if(){
+  // 
+  //   var Unit_String = "";
+  // 
+  // }
+  // 
+  
+  
 }
 
-function Calculating_Tolerance(){
-  
-  
-  
+function Calculate_Tolerance(Tolerance_Letter){
+  var Tolerance_Dictionary = {
+  "B": "Tolerance: ± 0.1%",
+  "C": "Tolerance: ± 0.25%",
+  "D": "Tolerance: ± 0.5%",
+  "F": "Tolerance: ± 1%",
+  "G": "Tolerance: ± 2%",
+  "J": "Tolerance: ± 5%",
+  "K": "Tolerance: ± 10%",
+  "M": "Tolerance: ± 20%",
+  "Z": "Tolerance: - 20% to + 80% "};
+  var Tolerance_Value = Tolerance_Dictionary[Tolerance_Letter];
+  document.getElementsByClassName('Tolerance_Value')[0].value = Tolerance_Value;  
 }
 
 
