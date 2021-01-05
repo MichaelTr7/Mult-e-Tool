6cea1c5e5c6cb98e7688d1d7ea393762e8f3f2d2
Michael <mailmichaeltr@gmail.com>
2021-01-05 15:07:58 -0500

    Carousel Update

--- JavaScript/Digital_Logic_Gates.js
+++ JavaScript/Digital_Logic_Gates.js
@@ -15,7 +15,38 @@ window.onload = function(){
   var Top_Bar_Wrapper = document.getElementsByClassName('Top_Bar')[0];
   Top_Bar_Wrapper.addEventListener("mouseleave",Left_Navigation_Bar);
 
+  var Indicator_Buttons = document.getElementsByClassName('Not_Active');
+  for(Index = 0; Index < Indicator_Buttons.length; Index++){
+    Indicator_Buttons[Index].addEventListener("mouseenter",Display_Gate_Name);
+    Indicator_Buttons[Index].addEventListener("mouseleave",Hide_Gate_Name);
+  }
+
+
   Adjust_Mobile_Menu();
-  
   $('.carousel').carousel({interval: false});
 }
+
+function Display_Gate_Name(){
+  var Gate_Label = document.getElementsByClassName('Logic_Gate_Label')[0];
+  var Gate_Names = ["AND","NAND","OR","NOR","XOR","XNOR","NOT","BUFFER"];
+  var Indicator_Index = parseInt((String(this.id)).split("_")[1]);
+  Gate_Label.innerHTML = Gate_Names[Indicator_Index-1];
+  Gate_Label.classList.add('Logic_Gate_Label_Active');
+
+}
+  
+function Hide_Gate_Name(){
+  var Gate_Label = document.getElementsByClassName('Logic_Gate_Label')[0];
+  Gate_Label.classList.remove('Logic_Gate_Label_Active');
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
