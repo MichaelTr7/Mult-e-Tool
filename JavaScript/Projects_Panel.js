a7646b50bbb822f3f35c54ed63faf2650778755d
Michael <mailmichaeltr@gmail.com>
2021-02-08 23:19:10 -0500

    List Adding Functionality

--- JavaScript/Projects_Panel.js
+++ JavaScript/Projects_Panel.js
@@ -25,6 +25,8 @@ window.onload = function(){
   document.getElementById('Logout_Button').addEventListener("click",Logout_Pressed);
   document.getElementById('New_Project_Button').addEventListener("click",New_Project_Pressed);
   document.getElementById('Trash_Button').addEventListener("click",Trash_Pressed);  
+  document.getElementById('Project_Name_Field').addEventListener("click",Project_Name_Field_Pressed);
+  document.getElementById('OK_Button').addEventListener("click",Set_Project_Name);
   document.addEventListener("keydown",Shift_Key_Pressed);
   document.addEventListener("keyup",Shift_Key_Released);
   document.addEventListener("keydown",Delete_Key_Pressed);
@@ -96,6 +98,10 @@ function New_Project_Pressed(){
     New_Open_Button_Element.innerHTML = "Open";
     New_Open_Button_Element.addEventListener("click",Open_Project);
   }  
+  
+    var Naming_Modal = document.getElementById('Modal_Backdrop');
+    Naming_Modal.classList.remove('Fade_Out_Rename_Modal_Animation');
+    Naming_Modal.style.display = "block";
 }
 
 function Trash_Pressed(){
@@ -194,11 +200,26 @@ function Delete_Key_Pressed(e){
 function Edit_Project_Name(){
   var Sibling_Label = this.previousSibling;
   console.log(Sibling_Label);
-  
+}
+
+function Project_Name_Field_Pressed(){
   
 }
 
-
+function Set_Project_Name(){
+  var Name = String(document.getElementById('Project_Name_Field').value);
+  if(Name != ""){
+    var List_Elements = document.getElementsByClassName('Project_Rows');
+    var Newly_Added_Element = List_Elements[parseInt(List_Elements.length)-1]
+    var Modal = document.getElementById('Modal_Backdrop');
+    Modal.classList.add('Fade_Out_Rename_Modal_Animation');
+    var Animation_Duration = parseFloat(getComputedStyle(Modal).getPropertyValue('--Fade_Out_Duration'))*1000;
+    setTimeout(function () {
+      Modal.style.display = "none";
+    }, Animation_Duration);    
+    Newly_Added_Element.children[1].innerHTML = Name;
+  }
+}
 
 
 
