5bb9d3f26d044cc3f6f08af2c3546c5aa49d49e0
Michael <mailmichaeltr@gmail.com>
2021-05-06 15:07:13 -0400

    Disabled Service Worker

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-if("serviceWorker" in navigator){
-  navigator.serviceWorker.register("sw.js").then(registration => {
-    console.log("Service Worker Registered");
-    console.log(registration);
-  }).catch(error => {
-    console.log("Service Worker Registered Failed")
-    console.log(error);
-  });  
-}
+// if("serviceWorker" in navigator){
+//   navigator.serviceWorker.register("sw.js").then(registration => {
+//     console.log("Service Worker Registered");
+//     console.log(registration);
+//   }).catch(error => {
+//     console.log("Service Worker Registered Failed")
+//     console.log(error);
+//   });  
+// }
--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener("install", e => {
-    console.log("Install");
-})
+// self.addEventListener("install", e => {
+//     console.log("Install");
+// })
